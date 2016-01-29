fee6eb3222072cb343f6b3ff44cd86d663ed522f
say-hanabi <say-hanabi@users.noreply.github.com>
2016-01-28 23:35:29 -0500

    Experiment with side-wide effect.

--- source/plugin/cdc_html5eff/effect.class.php
+++ source/plugin/cdc_html5eff/effect.class.php
@@ -6,7 +6,7 @@ class plugin_cdc_html5eff {
 		function global_cpnav_top() {
 			return '<link rel="stylesheet" type="text/css" href="source/plugin/cdc_html5eff/images/html5.css"/>';
 		}
-		function global_usernav_extra2() {
+		function global_header() {
 			return '<link rel="stylesheet" type="text/css" href="source/plugin/cdc_html5eff/images/html5.css"/>';
 		}
 }
