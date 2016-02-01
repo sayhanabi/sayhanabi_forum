d1396e7085374133e1dda93d2a9057d5de42d582
say-hanabi <say-hanabi@users.noreply.github.com>
2016-01-31 22:15:08 -0500

    Update background.js

--- static/js/background.js
+++ static/js/background.js
@@ -2,7 +2,7 @@ var totalImages = 5; //总图片数量
 var defaultImage = 5; //默认图片，填编号
 var cssAssets = [
     [
-        ["forum", "thread", "home::spacecp", "home::task", "home::medal", "portal", "userapp", "cp", "search"],
+        ["forum", "thread", "home::spacecp", "home::task", "home::medal", "home::magic", "portal", "userapp", "cp", "search"],
         ["bm_c", "pl", "ct2_a", "ct1", "frame"]
     ],
     [
